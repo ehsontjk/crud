3449e8bd4d38b75b6904a6dbfc5aec2da3287352
Ekhsondzhon Boboev <ehsonboboev@gmail.com>
2020-12-22 12:08:31 +0500

    v2

--- cmd/app/customers.go
+++ cmd/app/customers.go
@@ -10,35 +10,35 @@ import (
 
 func (s *Server) handleCustomerRegistration(w http.ResponseWriter, r *http.Request) {
 	
-	//обявляем структура клиента для запраса
+	
 	var item *customers.Customer
 
 	if err := json.NewDecoder(r.Body).Decode(&item); err != nil {
-		//вызываем фукцию для ответа с ошибкой
+		
 		errorWriter(w, http.StatusBadRequest, err)
 		return
 	}
 
-	//Генерируем bcrypt хеш от реалного пароля
+
 	hashed, err := bcrypt.GenerateFromPassword([]byte(item.Password), bcrypt.DefaultCost)
 	if err != nil {
-		//вызываем фукцию для ответа с ошибкой
+	
 		errorWriter(w, http.StatusInternalServerError, err)
 		return
 	}
-	//и поставляем хеш в поле парол
+
 	item.Password = string(hashed)
 
-	//сохроняем или обновляем клиент
+	
 	customer, err := s.customerSvc.Save(r.Context(), item)
 
-	//если получили ошибку то отвечаем с ошибкой
+	
 	if err != nil {
-		//вызываем фукцию для ответа с ошибкой
+		
 		errorWriter(w, http.StatusInternalServerError, err)
 		return
 	}
-	//вызываем функцию для ответа в формате JSON
+	
 	respondJSON(w, customer)
 }
 
--- cmd/app/managers.go
+++ cmd/app/managers.go
@@ -12,25 +12,25 @@ import (
 	"github.com/ehsontjk/crud/pkg/managers"
 )
 
-//ADMIN ...
+
 const ADMIN = "ADMIN"
 
 func (s *Server) handleManagerRegistration(w http.ResponseWriter, r *http.Request) {
 
 	id, err := middleware.Authentication(r.Context())
 	if err != nil {
-		//вызываем фукцию для ответа с ошибкой
+		
 		errorWriter(w, http.StatusBadRequest, err)
 		return
 	}
 	if id == 0 {
-		//вызываем фукцию для ответа с ошибкой
+	
 		errorWriter(w, http.StatusForbidden, err)
 		return
 	}
 
 	if !s.managerSvc.IsAdmin(r.Context(), id) {
-		//вызываем фукцию для ответа с ошибкой
+		
 		errorWriter(w, http.StatusForbidden, err)
 		return
 	}
@@ -45,7 +45,7 @@ func (s *Server) handleManagerRegistration(w http.ResponseWriter, r *http.Reques
 	err = json.NewDecoder(r.Body).Decode(&regItem)
 
 	if err != nil {
-		//вызываем фукцию для ответа с ошибкой
+		
 		errorWriter(w, http.StatusInternalServerError, err)
 		return
 	}
@@ -65,7 +65,7 @@ func (s *Server) handleManagerRegistration(w http.ResponseWriter, r *http.Reques
 	tkn, err := s.managerSvc.Create(r.Context(), item)
 
 	if err != nil {
-		//вызываем фукцию для ответа с ошибкой
+		
 		errorWriter(w, http.StatusInternalServerError, err)
 		return
 	}
@@ -80,14 +80,14 @@ func (s *Server) handleManagerGetToken(w http.ResponseWriter, r *http.Request) {
 	err := json.NewDecoder(r.Body).Decode(&manager)
 
 	if err != nil {
-		//вызываем фукцию для ответа с ошибкой
+		
 		errorWriter(w, http.StatusInternalServerError, err)
 		return
 	}
 
 	tkn, err := s.managerSvc.Token(r.Context(), manager.Phone, manager.Password)
 	if err != nil {
-		//вызываем фукцию для ответа с ошибкой
+		
 		errorWriter(w, http.StatusBadRequest, err)
 		return
 	}
@@ -98,12 +98,12 @@ func (s *Server) handleManagerGetToken(w http.ResponseWriter, r *http.Request) {
 func (s *Server) handleManagerChangeProducts(w http.ResponseWriter, r *http.Request) {
 	id, err := middleware.Authentication(r.Context())
 	if err != nil {
-		//вызываем фукцию для ответа с ошибкой
+		
 		errorWriter(w, http.StatusBadRequest, err)
 		return
 	}
 	if id == 0 {
-		//вызываем фукцию для ответа с ошибкой
+		
 		errorWriter(w, http.StatusForbidden, err)
 		return
 	}
@@ -111,14 +111,14 @@ func (s *Server) handleManagerChangeProducts(w http.ResponseWriter, r *http.Requ
 	err = json.NewDecoder(r.Body).Decode(&product)
 	fmt.Print(product)
 	if err != nil {
-		//вызываем фукцию для ответа с ошибкой
+		
 		errorWriter(w, http.StatusInternalServerError, err)
 		return
 	}
 
 	product, err = s.managerSvc.SaveProduct(r.Context(), product)
 	if err != nil {
-		//вызываем фукцию для ответа с ошибкой
+		
 		errorWriter(w, http.StatusInternalServerError, err)
 		return
 	}
@@ -129,12 +129,12 @@ func (s *Server) handleManagerChangeProducts(w http.ResponseWriter, r *http.Requ
 func (s *Server) handleManagerMakeSales(w http.ResponseWriter, r *http.Request) {
 	id, err := middleware.Authentication(r.Context())
 	if err != nil {
-		//вызываем фукцию для ответа с ошибкой
+		
 		errorWriter(w, http.StatusBadRequest, err)
 		return
 	}
 	if id == 0 {
-		//вызываем фукцию для ответа с ошибкой
+		
 		errorWriter(w, http.StatusForbidden, err)
 		return
 	}
@@ -143,14 +143,14 @@ func (s *Server) handleManagerMakeSales(w http.ResponseWriter, r *http.Request) 
 	err = json.NewDecoder(r.Body).Decode(&sale)
 
 	if err != nil {
-		//вызываем фукцию для ответа с ошибкой
+	
 		errorWriter(w, http.StatusInternalServerError, err)
 		return
 	}
 
 	sale, err = s.managerSvc.MakeSale(r.Context(), sale)
 	if err != nil {
-		//вызываем фукцию для ответа с ошибкой
+	
 		errorWriter(w, http.StatusBadRequest, err)
 		return
 	}
@@ -162,18 +162,18 @@ func (s *Server) handleManagerMakeSales(w http.ResponseWriter, r *http.Request) 
 func (s *Server) handleManagerGetSales(w http.ResponseWriter, r *http.Request) {
 	id, err := middleware.Authentication(r.Context())
 	if err != nil {
-		//вызываем фукцию для ответа с ошибкой
+		
 		errorWriter(w, http.StatusBadRequest, err)
 		return
 	}
 	if id == 0 {
-		//вызываем фукцию для ответа с ошибкой
+		
 		errorWriter(w, http.StatusForbidden, err)
 		return
 	}
 	total, err := s.managerSvc.GetSales(r.Context(), id)
 	if err != nil {
-		//вызываем фукцию для ответа с ошибкой
+	
 		errorWriter(w, http.StatusBadRequest, err)
 		return
 	}
@@ -186,7 +186,7 @@ func (s *Server) handleManagerGetProducts(w http.ResponseWriter, r *http.Request
 
 	items, err := s.managerSvc.Products(r.Context())
 	if err != nil {
-		//вызываем фукцию для ответа с ошибкой
+		
 		errorWriter(w, http.StatusBadRequest, err)
 		return
 	}
@@ -198,31 +198,31 @@ func (s *Server) handleManagerGetProducts(w http.ResponseWriter, r *http.Request
 func (s *Server) handleManagerRemoveProductByID(w http.ResponseWriter, r *http.Request) {
 	id, err := middleware.Authentication(r.Context())
 	if err != nil {
-		//вызываем фукцию для ответа с ошибкой
+		
 		errorWriter(w, http.StatusBadRequest, err)
 		return
 	}
 	if id == 0 {
-		//вызываем фукцию для ответа с ошибкой
+		
 		errorWriter(w, http.StatusForbidden, err)
 		return
 	}
 
 	idParam, ok := mux.Vars(r)["id"]
 	if !ok {
-		//вызываем фукцию для ответа с ошибкой
+		
 		errorWriter(w, http.StatusBadRequest, errors.New("Missing id"))
 		return
 	}
 	productID, err := strconv.ParseInt(idParam, 10, 64)
 	if err != nil {
-		//вызываем фукцию для ответа с ошибкой
+		
 		errorWriter(w, http.StatusBadRequest, err)
 		return
 	}
 	err = s.managerSvc.RemoveProductByID(r.Context(), productID)
 	if err != nil {
-		//вызываем фукцию для ответа с ошибкой
+		
 		errorWriter(w, http.StatusBadRequest, err)
 		return
 	}
@@ -232,31 +232,31 @@ func (s *Server) handleManagerRemoveProductByID(w http.ResponseWriter, r *http.R
 func (s *Server) handleManagerRemoveCustomerByID(w http.ResponseWriter, r *http.Request) {
 	id, err := middleware.Authentication(r.Context())
 	if err != nil {
-		//вызываем фукцию для ответа с ошибкой
+		
 		errorWriter(w, http.StatusBadRequest, err)
 		return
 	}
 	if id == 0 {
-		//вызываем фукцию для ответа с ошибкой
+		
 		errorWriter(w, http.StatusForbidden, err)
 		return
 	}
 
 	idParam, ok := mux.Vars(r)["id"]
 	if !ok {
-		//вызываем фукцию для ответа с ошибкой
+		
 		errorWriter(w, http.StatusBadRequest, errors.New("Missing id"))
 		return
 	}
 	customerID, err := strconv.ParseInt(idParam, 10, 64)
 	if err != nil {
-		//вызываем фукцию для ответа с ошибкой
+		
 		errorWriter(w, http.StatusBadRequest, err)
 		return
 	}
 	err = s.managerSvc.RemoveCustomerByID(r.Context(), customerID)
 	if err != nil {
-		//вызываем фукцию для ответа с ошибкой
+	
 		errorWriter(w, http.StatusBadRequest, err)
 		return
 	}
@@ -266,19 +266,19 @@ func (s *Server) handleManagerRemoveCustomerByID(w http.ResponseWriter, r *http.
 func (s *Server) handleManagerGetCustomers(w http.ResponseWriter, r *http.Request) {
 	id, err := middleware.Authentication(r.Context())
 	if err != nil {
-		//вызываем фукцию для ответа с ошибкой
+		
 		errorWriter(w, http.StatusBadRequest, err)
 		return
 	}
 	if id == 0 {
-		//вызываем фукцию для ответа с ошибкой
+		
 		errorWriter(w, http.StatusForbidden, err)
 		return
 	}
 
 	items, err := s.managerSvc.Customers(r.Context())
 	if err != nil {
-		//вызываем фукцию для ответа с ошибкой
+	
 		errorWriter(w, http.StatusBadRequest, err)
 		return
 	}
@@ -290,12 +290,12 @@ func (s *Server) handleManagerGetCustomers(w http.ResponseWriter, r *http.Reques
 func (s *Server) handleManagerChangeCustomer(w http.ResponseWriter, r *http.Request) {
 	id, err := middleware.Authentication(r.Context())
 	if err != nil {
-		//вызываем фукцию для ответа с ошибкой
+	
 		errorWriter(w, http.StatusBadRequest, err)
 		return
 	}
 	if id == 0 {
-		//вызываем фукцию для ответа с ошибкой
+		
 		errorWriter(w, http.StatusForbidden, err)
 		return
 	}
@@ -303,14 +303,14 @@ func (s *Server) handleManagerChangeCustomer(w http.ResponseWriter, r *http.Requ
 	err = json.NewDecoder(r.Body).Decode(&customer)
 	fmt.Println(customer)
 	if err != nil {
-		//вызываем фукцию для ответа с ошибкой
+		
 		errorWriter(w, http.StatusInternalServerError, err)
 		return
 	}
 
 	customer, err = s.managerSvc.ChangeCustomer(r.Context(), customer)
 	if err != nil {
-		//вызываем фукцию для ответа с ошибкой
+		
 		errorWriter(w, http.StatusBadRequest, err)
 		return
 	}
